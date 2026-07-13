0d8aa8b9892a598deba9bab30db6d8d545ba5c78
Shubhika Sinha <151361248+shubhikasinha@users.noreply.github.com>
2026-07-13 12:50:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (37.33%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        ans = []
+        def preorder(node):
+            if node is None:
+                return
+            
+            ans.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
+
+        preorder(root)
+        return ans
+       
+        
